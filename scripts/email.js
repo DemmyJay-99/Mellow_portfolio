8a976bc05b3a91da90760055804a40cd6d8c0c9a
DemmyJay <demmyjyd@gmail.com>
2025-08-02 00:41:39 +0100

    Add fancy loader to contact form

--- scripts/email.js
+++ scripts/email.js
@@ -1,3 +1,36 @@
+const btn = document.getElementById('btn');
+const span = document.getElementById('span');
+const container = document.getElementById('container');
+
+
+function showLoader() {
+    container.style.display ='block';
+    span.style.display = 'none';
+}
+
+function hideLoader() {
+    container.style.display ='none';
+    span.style.display = 'block';  
+}
+
+function disableForm() {
+    btn.disabled = true;
+}
+
+function enableForm() {
+    btn.disabled = false;
+}
+
+
+
+
+
+
+
+
+
+
+
 function sendEmail() {
     const templateParams = {
         name : document.getElementById('name').value,
@@ -6,9 +39,13 @@ function sendEmail() {
         message : document.getElementById('message').value,
     };
 
+    showLoader();
+    disableForm();
+
+
 emailjs.send("service_4uxyt5c", "template_ven3kqr", templateParams)
-.then(
-    () => alert("Email sent!").catch(() => alert("Email not sent!"))
-);
+.then(() => alert("Email sent!")).catch(() => alert("Email not sent!")).finally(() => {
+    hideLoader();
+    enableForm();})
 }
 
